794592a9a79c1a335b4a86bcd1c631d4af47d721
razorkitty <razorkitty@null.net>
2024-12-08 04:06:58 +0000

    updated config

--- lua/options.lua
+++ lua/options.lua
@@ -4,25 +4,25 @@ require "nvchad.options"
 
 local o = vim.o
 --o.cursorlineopt ='both' -- to enable cursorline!
-o.foldmethod = "expr"
-o.foldexpr = "nvim_treesitter#foldexpr()"
-o.foldcolumn = "auto:8"
-o.fillchars = "foldopen:,foldclose:,fold: "
-
-
--- force folds to be regenerated on file changes
--- TODO: find a more elegant way of handling this
-vim.api.nvim_create_autocmd(
-  {
-    'CursorHoldI',
-    'BufEnter',
-    'BufLeave',
-    'InsertLeave',
-    'CompleteDone'
-  },
-  {
-    callback = function ()
-      o.foldexpr = "nvim_treesitter#foldexpr()"
-    end
-  }
-)
+--o.foldmethod = "expr"
+--o.foldexpr = "nvim_treesitter#foldexpr()"
+--o.foldcolumn = "auto:8"
+--o.fillchars = "foldopen:,foldclose:,fold: "
+--
+--
+---- force folds to be regenerated on file changes
+---- TODO: find a more elegant way of handling this
+--vim.api.nvim_create_autocmd(
+--  {
+--    'CursorHoldI',
+--    'BufEnter',
+--    'BufLeave',
+--    'InsertLeave',
+--    'CompleteDone'
+--  },
+--  {
+--    callback = function ()
+--      o.foldexpr = "nvim_treesitter#foldexpr()"
+--    end
+--  }
+--)
