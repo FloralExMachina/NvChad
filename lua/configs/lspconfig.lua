3ac7e60eabe5ceac83b8bb00b7877858d11c5917
razorkitty <razorkitty@null.net>
2024-12-10 05:41:13 +0000

    updated config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "intelephense", "clangd", "marksman" }
+local servers = { "html", "cssls", "intelephense", "clangd", "markdown_oxide" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
